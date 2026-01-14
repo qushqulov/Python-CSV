61ed00e44c83dec8c43ef7ec518f95cb0bd0587c
qushqulov <qushqulovolmasbek25@gmail.com>
2026-01-14 22:27:50 +0500

    fayl yuklandi

--- rating_sorter.py
+++ rating_sorter.py
@@ -1,1 +1,30 @@
+f = open("students.csv", "r")
+lines = f.readlines()
+f.close()
 
+lines = lines[1:]
+
+students = []
+
+for line in lines:
+    line = line.strip()
+    name, score = line.split(",")
+    students.append({
+        "name": name,
+        "score": int(score)
+    })
+
+for i in range(len(students)):
+    for j in range(i + 1, len(students)):
+        if students[i]["score"] < students[j]["score"]:
+            students[i], students[j] = students[j], students[i]
+
+f = open("rating.csv", "w")
+f.write("rank,name,score\n")
+
+rank = 1
+for student in students:
+    f.write(f"{rank},{student['name']},{student['score']}\n")
+    rank += 1
+
+f.close()
